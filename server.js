3470b42ee225d938238bf5c851417c45d4ba5922
ioncrisa2 <zyohanes99@gmail.com>
2022-08-21 00:07:11 +0700

    finished

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-import Hapi from '@hapi/hapi';
-import { routes } from './src/routes.js';
+const Hapi = require('@hapi/hapi');
+const routes = require('./src/routes.js');
 
 const init = async () => {
     const server = Hapi.server({
         port:5000,
-        host:'localhost'
+        host:'localhost',
+        routes:{
+            cors:{
+                origin: ['*']
+            }
+        }
     });
 
-    server.route(routes)
+    server.route(routes);
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
--- src/routes.js
+++ src/routes.js
@@ -1,75 +1,37 @@
-export const routes = [
-    {
-        method:'GET',
-        path:'/',
-        handler:(request, h) => {
-            return 'homepage';
-        }
-    },
-    {
-        method:'*',
-        path:'/',
-        handler:(request, h) => {
-            return `Halaman tidak dapat diakses dengan method tersebut!!`
-        }
-    },
-    {
-        method:'GET',
-        path:'/about',
-        handler:(request, h) => {
-            return 'about page';
-        }
-    },
-    {
-        method:'*',
-        path:'/about',
-        handler:(request, h) => {
-            return 'Halaman tidak dapat diakses dengan method tersebut!!'
-        }
-    },
-    {
-        method:'*',
-        path:'/{any}',
-        handler:(request, h) => {
-            return 'Halaman tidak ditemukan!'
-        }
-    },
-    {
-        method:'GET',
-        path:'/users/{name}/{username?}',
-        handler:(request, h) => {
-            const {username = undefined, name } = request.params;
-            return `Hello ${name}, your username is ${username}`;
-        }
-    },
-    {
-        method:'GET',
-        path:'/hello/{name?}',
-        handler: (request, h) => {
-            const {name = 'stranger'} = request.params;
-            const {lang} = request.query;
+const { 
+    addNoteHandler, 
+    getAllNotesHandler, 
+    getNotesByIdHandler, 
+    editNoteByIdHandler, 
+    deleteNoteByIdHandler
+} = require('./handler.js');
 
-            if(lang === 'id'){
-                return `Hai, ${name}`;
-            }
-
-            return `Hello ${name}`;
-        }
-    },
-    {
-        method:'POST',
-        path:'/login',
-        handler:(request, h) => {
-            const {username, password} = request.payload;
-            return `Welcome ${username}`;
-        }
-    },
-    {
-        method:'POST',
-        path:'/user',
-        handler:(request, h) => {
-            return h.response('created').code(201).header('application/json');
-        }
-    }
+const routes = [
+   {
+    method:'POST',
+    path:'/notes',
+    handler: addNoteHandler
+   },
+   {
+    method:'GET',
+    path:'/notes',
+    handler: getAllNotesHandler
+   },
+   {
+    method:'GET',
+    path:'/notes/{id}',
+    handler: getNotesByIdHandler
+   },
+   {
+    method:'PUT',
+    path:'/notes/{id}',
+    handler: editNoteByIdHandler
+   },
+   {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler
+   }
 ];
 
+module.exports = routes;
